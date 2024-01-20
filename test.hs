4c25ced81f9d0ecf5129e263b63345faaf5d91bc
mug4z <mug4z@protonmail.com>
2024-01-20 20:30:14 +0100

    removed comment

--- test.hs
+++ test.hs
@@ -1,5 +1,5 @@
-printparam :: String -> String -> String
-printparam print premierNom
-printparam print deuxième
-    where premierNom = "Tim"
-          deuxiemeNom = "Loic"
+cylinder :: Double -> Double -> Double
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea  = pi * r ^ 2
+    in sideArea + 2 * topArea
